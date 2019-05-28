1ae6aedc341ccc62b73acd970c3d933fe2dd61ad
Vathunyoo_Uraisakun <vathunyoo.uraisakun@gmail.com>
2019-05-28 17:58:48 +0700

    Finish V3
    - Add Api

--- kotlin-source/src/main/kotlin/com/example/api/ExampleApi.kt
+++ kotlin-source/src/main/kotlin/com/example/api/ExampleApi.kt
@@ -1,7 +1,6 @@
 package com.example.api
 
-import com.example.flow.BondFlow_Issue
-import com.example.flow.ExampleFlow
+import com.example.flow.*
 import com.example.flow.ExampleFlow.Initiator
 import com.example.schema.IOUSchemaV1
 import com.example.state.BlacklistState
@@ -24,6 +23,7 @@ import net.corda.core.utilities.loggerFor
 import net.corda.finance.contracts.asset.Cash
 import net.corda.finance.contracts.getCashBalances
 import net.corda.finance.flows.CashIssueFlow
+import net.corda.finance.flows.CashPaymentFlow
 import org.slf4j.Logger
 import java.time.Instant
 import java.util.*
@@ -77,84 +77,10 @@ class ExampleApi(private val rpcOps: CordaRPCOps) {
         cashObj1?.quatity = thbQuatity
         return listCash[0]
     }
-
     @GET
     @Path("cash")
     @Produces(MediaType.APPLICATION_JSON)
     fun getCash() = rpcOps.vaultQuery(Cash.State::class.java).states
-
-    @PUT
-    @Path("Issue_cash")
-    fun issueCash(@QueryParam("amount") amount: Int, @QueryParam("currency") currency: String): Response {
-
-        var amountCash = Amount<Currency>(amount.toLong(),Currency.getInstance(currency))
-//        var partyCash = Party()
-        return try {
-            val signedTx = rpcOps.startTrackedFlow(CashIssueFlow, amountCash, 0,).returnValue.getOrThrow()
-            Response.status(CREATED).entity("Transaction id ${signedTx.id} committed to ledger.\n").build()
-
-        } catch (ex: Throwable) {
-            logger.error(ex.message, ex)
-            Response.status(BAD_REQUEST).entity(ex.message!!).build()
-        }
-    }
-
-    // --------------------------
-    // All bond state
-    // --------------------------
-    @GET
-    @Path("bond")
-    @Produces(MediaType.APPLICATION_JSON)
-    fun getBond() = rpcOps.vaultQueryBy<BondState>().states
-
-    // --------------------------
-    // All blacklist state
-    // --------------------------
-    @GET
-    @Path("blacklist")
-    @Produces(MediaType.APPLICATION_JSON)
-    fun getBlacklist() = rpcOps.vaultQueryBy<BlacklistState>().states
-
-    @GET
-    @Path("peers")
-    @Produces(MediaType.APPLICATION_JSON)
-    fun getPeers(): Map<String, List<CordaX500Name>> {
-        val nodeInfo = rpcOps.networkMapSnapshot()
-        return mapOf("peers" to nodeInfo
-                .map { it.legalIdentities.first().name }
-                //filter out myself, notary and eventual network map started by driver
-                .filter { it.organisation !in (SERVICE_NAMES + myLegalName.organisation) })
-    }
-
-
-    @GET
-    @Path("ious")
-    @Produces(MediaType.APPLICATION_JSON)
-    fun getIOUs() = rpcOps.vaultQueryBy<IOUState>().states
-
-    @PUT
-    @Path("create-iou")
-    fun createIOU(@QueryParam("iouValue") iouValue: Int, @QueryParam("partyName") partyName: CordaX500Name?): Response {
-        if (iouValue <= 0) {
-            return Response.status(BAD_REQUEST).entity("Query parameter 'iouValue' must be non-negative.\n").build()
-        }
-        if (partyName == null) {
-            return Response.status(BAD_REQUEST).entity("Query parameter 'partyName' missing or has wrong format.\n").build()
-        }
-        val otherParty = rpcOps.wellKnownPartyFromX500Name(partyName)
-                ?: return Response.status(BAD_REQUEST).entity("Party named $partyName cannot be found.\n").build()
-
-        return try {
-            val signedTx = rpcOps.startTrackedFlow(ExampleFlow::Initiator, iouValue, otherParty).returnValue.getOrThrow()
-            Response.status(CREATED).entity("Transaction id ${signedTx.id} committed to ledger.\n").build()
-
-        } catch (ex: Throwable) {
-            logger.error(ex.message, ex)
-            Response.status(BAD_REQUEST).entity(ex.message!!).build()
-        }
-    }
-
-
 
     @GET
     @Path("cash-balances")
@@ -162,7 +88,7 @@ class ExampleApi(private val rpcOps: CordaRPCOps) {
     fun getCashBalances() = rpcOps.getCashBalances()
 
     @PUT
-    @Path("self-issue-cash")
+    @Path("issue-cash")
     fun selfIssueCash(@QueryParam(value = "amount") amount: Int,
                       @QueryParam(value = "currency") currency: String): Response {
 
@@ -184,6 +110,205 @@ class ExampleApi(private val rpcOps: CordaRPCOps) {
         // 3. Return the response.
         return Response.status(status).entity(message).build()
     }
+
+    @PUT
+    @Path("payment-cash")
+    fun selfIssueCash(@QueryParam(value = "amount") amount: Int,
+                      @QueryParam(value = "currency") currency: String,
+                      @QueryParam("paymentName") paymentName: CordaX500Name
+    ): Response {
+
+        // 1. Prepare issue request.
+        val paymentAmount = Amount(amount.toLong() * 100, Currency.getInstance(currency))
+        var paymentParty = rpcOps.wellKnownPartyFromX500Name(paymentName)
+                ?: return Response.status(BAD_REQUEST).entity("Party named $paymentName cannot be found.\n").build()
+
+        // 2. Start flow and wait for response.
+        val (status, message) = try {
+            val flowHandle = rpcOps.startFlowDynamic(CashPaymentFlow::class.java, paymentAmount,paymentParty)
+            val result = flowHandle.use { it.returnValue.getOrThrow() }
+            CREATED to result.stx.tx.outputs.single().data
+        } catch (e: Exception) {
+            BAD_REQUEST to e.message
+        }
+
+        // 3. Return the response.
+        return Response.status(status).entity(message).build()
+    }
+
+    // --------------------------
+    // All bond state
+    // --------------------------
+    @GET
+    @Path("bond")
+    @Produces(MediaType.APPLICATION_JSON)
+    fun getBond() = rpcOps.vaultQueryBy<BondState>().states
+
+    @PUT
+    @Path("create-bond")
+    fun createBond(@QueryParam("amount") amount: Int,
+                  @QueryParam("currency") currency: String,
+                  @QueryParam("lender") lender: CordaX500Name?,
+                  @QueryParam("escrow") escrow: CordaX500Name?,
+                  @QueryParam("interest") interest: Double,
+                  @QueryParam("period") period: Int
+    ): Response {
+        val bondAmount = Amount(amount.toLong() * 100, Currency.getInstance(currency))
+        if (amount <= 0) {
+            return Response.status(BAD_REQUEST).entity("Query parameter 'amount' must be non-negative.\n").build()
+        }
+        if (lender == null) {
+            return Response.status(BAD_REQUEST).entity("Query parameter 'lender' missing or has wrong format.\n").build()
+        }
+        if (escrow == null) {
+            return Response.status(BAD_REQUEST).entity("Query parameter 'escrow' missing or has wrong format.\n").build()
+        }
+        if (interest <= 0) {
+            return Response.status(BAD_REQUEST).entity("Query parameter 'interest' must be non-negative.\n").build()
+        }
+        if (period <= 0) {
+            return Response.status(BAD_REQUEST).entity("Query parameter 'period' must be non-negative.\n").build()
+        }
+        var lenderParty = rpcOps.wellKnownPartyFromX500Name(lender)
+                ?: return Response.status(BAD_REQUEST).entity("Party named $lender cannot be found.\n").build()
+
+        var escrowParty = rpcOps.wellKnownPartyFromX500Name(escrow)
+                ?: return Response.status(BAD_REQUEST).entity("Party named $escrow cannot be found.\n").build()
+
+        return try {
+            val signedTx = rpcOps.startTrackedFlow(BondFlow_Issue::Initiator, bondAmount,lenderParty,escrowParty,interest,period,Instant.now().plusSeconds(10000)).returnValue.getOrThrow()
+            Response.status(CREATED).entity("Transaction id ${signedTx.id} committed to ledger.\n").build()
+
+        } catch (ex: Throwable) {
+            logger.error(ex.message, ex)
+            Response.status(BAD_REQUEST).entity(ex.message!!).build()
+        }
+    }
+
+    @PUT
+    @Path("update-bond")
+    fun updateBond(@QueryParam("amount") amount: Int,
+                   @QueryParam("currency") currency: String,
+                   @QueryParam("bondRef") bondRef: String
+    ): Response {
+        val bondAmount = Amount(amount.toLong() * 100, Currency.getInstance(currency))
+        if (amount <= 0) {
+            return Response.status(BAD_REQUEST).entity("Query parameter 'amount' must be non-negative.\n").build()
+        }
+
+        return try {
+            val signedTx = rpcOps.startTrackedFlow(BondFlow_Update::Initiator, bondAmount, bondRef).returnValue.getOrThrow()
+            Response.status(CREATED).entity("Transaction id ${signedTx.id} committed to ledger.\n").build()
+
+        } catch (ex: Throwable) {
+            logger.error(ex.message, ex)
+            Response.status(BAD_REQUEST).entity(ex.message!!).build()
+        }
+    }
+
+    @PUT
+    @Path("redeem-bond")
+    fun redeemBond(@QueryParam("bondRef") bondRef: String
+    ): Response {
+        return try {
+            val signedTx = rpcOps.startTrackedFlow(BondFlow_Redeem::Initiator, bondRef).returnValue.getOrThrow()
+            Response.status(CREATED).entity("Transaction id ${signedTx.id} committed to ledger.\n").build()
+
+        } catch (ex: Throwable) {
+            logger.error(ex.message, ex)
+            Response.status(BAD_REQUEST).entity(ex.message!!).build()
+        }
+    }
+
+    // --------------------------
+    // All blacklist state
+    // --------------------------
+    @GET
+    @Path("blacklist")
+    @Produces(MediaType.APPLICATION_JSON)
+    fun getBlacklist() = rpcOps.vaultQueryBy<BlacklistState>().states
+
+    @PUT
+    @Path("blacklist-issue")
+    fun issueBlacklist(
+            @QueryParam("blacklistName") blacklistName: CordaX500Name?,
+            @QueryParam("escrow") escrow: CordaX500Name?
+    ): Response {
+        if (blacklistName == null) {
+            return Response.status(BAD_REQUEST).entity("Query parameter 'blacklistName' missing or has wrong format.\n").build()
+        }
+        if (escrow == null) {
+            return Response.status(BAD_REQUEST).entity("Query parameter 'escrow' missing or has wrong format.\n").build()
+        }
+        var blacklistParty = rpcOps.wellKnownPartyFromX500Name(blacklistName)
+                ?: return Response.status(BAD_REQUEST).entity("Party named $blacklistName cannot be found.\n").build()
+
+        var escrowParty = rpcOps.wellKnownPartyFromX500Name(escrow)
+                ?: return Response.status(BAD_REQUEST).entity("Party named $escrow cannot be found.\n").build()
+        return try {
+            val signedTx = rpcOps.startTrackedFlow(Blacklist_Issue::Initiator, blacklistParty,escrowParty).returnValue.getOrThrow()
+            Response.status(CREATED).entity("Transaction id ${signedTx.id} committed to ledger.\n").build()
+
+        } catch (ex: Throwable) {
+            logger.error(ex.message, ex)
+            Response.status(BAD_REQUEST).entity(ex.message!!).build()
+        }
+    }
+
+    @PUT
+    @Path("blacklist-update")
+    fun issueBlacklist(
+            @QueryParam("blackRef") blackRef: String
+    ): Response {
+        return try {
+            val signedTx = rpcOps.startTrackedFlow(Blacklist_Update::Initiator, blackRef).returnValue.getOrThrow()
+            Response.status(CREATED).entity("Transaction id ${signedTx.id} committed to ledger.\n").build()
+
+        } catch (ex: Throwable) {
+            logger.error(ex.message, ex)
+            Response.status(BAD_REQUEST).entity(ex.message!!).build()
+        }
+    }
+
+    @GET
+    @Path("peers")
+    @Produces(MediaType.APPLICATION_JSON)
+    fun getPeers(): Map<String, List<CordaX500Name>> {
+        val nodeInfo = rpcOps.networkMapSnapshot()
+        return mapOf("peers" to nodeInfo
+                .map { it.legalIdentities.first().name }
+                //filter out myself, notary and eventual network map started by driver
+                .filter { it.organisation !in (SERVICE_NAMES + myLegalName.organisation) })
+    }
+
+
+    @GET
+    @Path("ious")
+    @Produces(MediaType.APPLICATION_JSON)
+    fun getIOUs() = rpcOps.vaultQueryBy<IOUState>().states
+
+    @POST
+    @Path("create-iou")
+    fun createIOU(@QueryParam("iouValue") iouValue: Int, @QueryParam("partyName") partyName: CordaX500Name?): Response {
+        if (iouValue <= 0) {
+            return Response.status(BAD_REQUEST).entity("Query parameter 'iouValue' must be non-negative.\n").build()
+        }
+        if (partyName == null) {
+            return Response.status(BAD_REQUEST).entity("Query parameter 'partyName' missing or has wrong format.\n").build()
+        }
+        val otherParty = rpcOps.wellKnownPartyFromX500Name(partyName)
+                ?: return Response.status(BAD_REQUEST).entity("Party named $partyName cannot be found.\n").build()
+
+        return try {
+            val signedTx = rpcOps.startTrackedFlow(ExampleFlow::Initiator, iouValue, otherParty).returnValue.getOrThrow()
+            Response.status(CREATED).entity("Transaction id ${signedTx.id} committed to ledger.\n").build()
+
+        } catch (ex: Throwable) {
+            logger.error(ex.message, ex)
+            Response.status(BAD_REQUEST).entity(ex.message!!).build()
+        }
+    }
+
 
     @GET
     @Path("issue-obligation")
@@ -215,52 +340,6 @@ class ExampleApi(private val rpcOps: CordaRPCOps) {
         return Response.status(status).entity(message).build()
     }
 
-    // Check this state for connect api
-//    @GET
-//    @Path("obligations")
-//    @Produces(MediaType.APPLICATION_JSON)
-//    fun obligations(): List<BondState> {
-//        val statesAndRefs = rpcOps.vaultQuery(BondState::class.java).states
-//        return statesAndRefs
-//                .map { stateAndRef -> stateAndRef.state.data }
-//                .map { state ->
-//                    // We map the anonymous lender and borrower to well-known identities if possible.
-//                    val possiblyWellKnownLender = rpcOps.wellKnownPartyFromAnonymous(state.lender) ?: state.lender
-//                    val possiblyWellKnownBorrower = rpcOps.wellKnownPartyFromAnonymous(state.owner) ?: state.owner
-//                    val possiblyWellKnownFinancial = rpcOps.wellKnownPartyFromAnonymous(state.escrow) ?: state.escrow
-//
-//                    BondState(state.amount,
-//                            possiblyWellKnownBorrower,
-//                            possiblyWellKnownLender,
-//                            possiblyWellKnownFinancial,
-//                            state.duedate,
-//                            state.linearId)
-//                }
-//    }
-
-//    @PUT
-//    @Path("Issue_bond")
-//    fun issuebond(@QueryParam("amount") iouValue: Int, @QueryParam("partyName") partyName: CordaX500Name?): Response {
-//        if (iouValue <= 100) {
-//            return Response.status(BAD_REQUEST).entity("Query parameter 'iouValue' must be non-negative.\n").build()
-//        }
-//        if (partyName == null) {
-//            return Response.status(BAD_REQUEST).entity("Query parameter 'partyName' missing or has wrong format.\n").build()
-//        }
-//        val otherParty = rpcOps.wellKnownPartyFromX500Name(partyName) ?:
-//        return Response.status(BAD_REQUEST).entity("Party named $partyName cannot be found.\n").build()
-//
-//        return try {
-//            val signedTx = rpcOps.startTrackedFlow(BondFlow_Issue::Initiator, iouValue, otherParty).returnValue.getOrThrow()
-//            Response.status(CREATED).entity("Transaction id ${signedTx.id} committed to ledger.\n").build()
-//
-//        } catch (ex: Throwable) {
-//            logger.error(ex.message, ex)
-//            Response.status(BAD_REQUEST).entity(ex.message!!).build()
-//        }
-//    }
-
-	
 	/**
      * Displays all IOU states that are created by Party.
      */
